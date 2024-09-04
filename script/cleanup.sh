a888a69ee1d6c245ea9eba3f771a1b9050e077cc
Abhijit Hota <abhihota025@gmail.com>
2024-09-04 17:01:21 +0530

    more linter/test fixes

--- script/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Remove /var/lib/warpbuild-agentd/settings.json
-sudo rm /var/lib/warpbuild-agentd/settings.json
-
-echo "Cleanup complete"
--- script/generate-from-openapi.sh
+++ script/generate-from-openapi.sh
@@ -25,35 +25,33 @@ if [ ! -f "$swagger_path" ]; then
   exit 1
 fi
 
-api_path=$(remove_trailing_slash $1)
+api_path=$(remove_trailing_slash "$1")
 echo "Removing existing sdk"
-rm -rf $api_path
-echo "Generating sdk"
-latest_version=$(openapi-generator-cli version)
+rm -rf "$api_path"
 echo "Generating sdk using typescript-fetch and schemas"
 
 openapi-generator-cli generate -i $swagger_path \
   -g typescript-fetch \
-  -o $api_path \
+  -o "$api_path" \
   --additional-properties=supportsES6=true \
   --additional-properties=paramNaming=snake_case \
   --additional-properties=enumPropertyNaming=snake_case \
   --additional-properties=modelPropertyNaming=snake_case \
   --additional-properties=typescriptThreePlus=true \
   --additional-properties=useSingleRequestParameter=true \
-  --additional-properties=withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api,npmName=@argonautdev/warpbuild-js-sdk,npmVersion=$1,legacyDiscriminatorBehavior=false,disallowAdditionalPropertiesIfNotPresent=false \
+  --additional-properties=withSeparateModelsAndApi=true,modelPackage=model,apiPackage=api,npmName=@argonautdev/warpbuild-js-sdk,npmVersion="$1",legacyDiscriminatorBehavior=false,disallowAdditionalPropertiesIfNotPresent=false \
   --enable-post-process-file \
   --skip-validate-spec
 
 echo "Removing existing 'schemas' directory"
-rm -rf $api_path/schemas
+rm -rf "$api_path/schemas"
 echo "Removed existing 'schemas' directory"
 
-rmdir $api_path/typescript-fetch || true
-rm $api_path/package.json || true
-rm $api_path/tsconfig.json || true
-rm $api_path/git_push.sh || true
-rm $api_path/README.md || true
-rm $api_path/.gitignore || true
-rm $api_path/.npmignore || true
+rmdir "$api_path/typescript-fetch" || true
+rm "$api_path/package.json" || true
+rm "$api_path/tsconfig.json" || true
+rm "$api_path/git_push.sh" || true
+rm "$api_path/README.md" || true
+rm "$api_path/.gitignore" || true
+rm "$api_path/.npmignore" || true
 
